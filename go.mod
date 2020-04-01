b53a240b4312b43007a32e9b563780a8c251b73f
zhaojf <zhaojf_xt@teamsun.com.cn>
2020-04-01 09:36:16 +0800

    add

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/glorinli/go-jwt-simple-auth
+
+go 1.13
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.7.4
+	github.com/jinzhu/gorm v1.9.12
+	golang.org/x/crypto v0.0.0-20200323165209-0ec3e9974c59
+)
--- models/base.go
+++ models/base.go
@@ -3,9 +3,7 @@ package models
 import (
 	"fmt"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"os"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var db *gorm.DB
@@ -13,14 +11,9 @@ var db *gorm.DB
 func init() {
 	var conn *gorm.DB
 	var err error
-	sqlite3DataPath := os.Getenv("go-jwt-simple-auth-sqlite3-path")
-	if sqlite3DataPath == "" {
-		sqlite3DataPath = "data.db"
-	}
 
-	fmt.Println("Use sqlite3: " + sqlite3DataPath)
-
-	conn, err = gorm.Open("sqlite3", sqlite3DataPath)
+	conn, err = gorm.Open("mysql",
+	"root:root@/gorest?charset=utf8")
 
 	if err != nil {
 		fmt.Print("Connect to db failed: " + err.Error())
